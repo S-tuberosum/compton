45cd53a034d79128df569ba8a0a29edf42ec4acc
S-tuberosum <okhaledn@gmail.com>
2021-10-05 17:14:11 -0500

    [Fitting] Partially fixed script, to be continued..

--- fit.C
+++ fit.C
@@ -9,40 +9,25 @@
 #include "TF1.h"
 #include "TLeaf.h"
 
-// double x_min = -451720;
-// double x_max = -45150;
+
+auto f1 = TF1("f1", "(x-[2])/[3]", -451720, -45150, "GL");
+
+Double_t a = 1/(1 + (4*2.33*7e9)/(510998.95*510998.95)); // kinematic factor
+
+auto aLong = TF1("aLong", "[1]*(1-x*(1+[0]))*((1-1/((1-x*(1-[0]))*(1-x*(1-[0]))))) / \
+(((x*(1-[0]))*(x*(1-[0])))/(1-x*(1-[0])) + 1 + ((1-x*(1+[0]))/(1-x*(1-[0]))* \
+((1-x*(1+[0]))/(1-x*(1-[0])))))", -451720, -45150, "GL"); // longitudinal asymmetry
+
+auto g = TF1("g", "aLong(f1(x))", -451720, -45150, "GL");
 
 void fit()
 {
-    //TFile *f = TFile::Open(input);
-    //TTree *t = (TTree*)f->Get("t");
-
-//     TLeaf* hitX = GetLeaf("det.2_hits.x");
-//     TLeaf* pXsec = GetLeaf("det.2_hits.x");
-//     TLeaf* uXsec = GetLeaf("uXsec");
-
-    //TGraph *a = new TGraph(?, det_2_hits_x, pXsec/uXsec);
-
-    //a->Draw();
-
-    auto f1 = TF1("f1", "(x-[2])/[3]", -451720, -45150);
-
     f1.SetParameter(2, -45150); // value of x where E = E_max 
-    f1.SetParameter(3, -45150 - -451720); // del x, difference between max and min of x
-
-    Double_t a = 1/(1 + (4*2.33*7e9)/(510998.95*510998.95)); // kinematic factor 
-
-    auto aLong = TF1("aLong", "[1]*(1-x*(1+[0]))*((1-1/((1-x*(1-[0]))*(1-x*(1-[0]))))) / \
-    (((x*(1-[0]))*(x*(1-[0])))/(1-x*(1-[0])) + 1 + ((1-x*(1+[0]))/(1-x*(1-[0]))* \
-    ((1-x*(1+[0]))/(1-x*(1-[0])))))", -451720, -45150); // longitudinal asymmetry 
+    f1.SetParameter(3, 220); // del x, difference between max and min of x
+    f1.GetParameter(2);
 
     aLong.SetParameter(0, a);
     aLong.SetParameter(1, 1); // polarization
 
-    auto g = TF1("g", "aLong(f1(x))");
-
-    //aa->Fit(g)
     //t->Draw("pXsec/uXsec:det.2_hits.x", "det.2_hits.x > -451720 & det.2_hits.x < -45150", "prof"); // profile plot of long asymmetry
-
 }
-
