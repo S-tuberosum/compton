f64b768509643dec0ce5d59ed5114dc4ffb6be61
S-tuberosum <okhaledn@gmail.com>
2021-12-11 15:31:43 -0600

    [Fitting] Working script implementation

--- fit.C
+++ fit.C
@@ -1,33 +1,34 @@
 // fitting our data to the predicted plot
 // assessing fit accuracy
 
-#include "TCanvas.h"
-#include "TObject.h"
-#include "TROOT.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TF1.h"
-#include "TLeaf.h"
-
-
-auto f1 = TF1("f1", "(x-[2])/[3]", -451720, -45150, "GL");
-
-Double_t a = 1/(1 + (4*2.33*7e9)/(510998.95*510998.95)); // kinematic factor
-
-auto aLong = TF1("aLong", "[1]*(1-x*(1+[0]))*((1-1/((1-x*(1-[0]))*(1-x*(1-[0]))))) / \
-(((x*(1-[0]))*(x*(1-[0])))/(1-x*(1-[0])) + 1 + ((1-x*(1+[0]))/(1-x*(1-[0]))* \
-((1-x*(1+[0]))/(1-x*(1-[0])))))", -451720, -45150, "GL"); // longitudinal asymmetry
-
-auto g = TF1("g", "aLong(f1(x))", -451720, -45150, "GL");
-
 void fit()
 {
-    f1.SetParameter(2, -45150); // value of x where E = E_max 
-    f1.SetParameter(3, 220); // del x, difference between max and min of x
-    f1.GetParameter(2);
+    int x_min = -451715;
+    int x_max = -451500;
 
-    aLong.SetParameter(0, a);
-    aLong.SetParameter(1, 1); // polarization
+    auto f1 = TF1("f1", "(x-[2])/[3]", x_min, x_max, "GL");
 
-    //t->Draw("pXsec/uXsec:det.2_hits.x", "det.2_hits.x > -451720 & det.2_hits.x < -45150", "prof"); // profile plot of long asymmetry
+    Double_t a = 1/(1 + (4*2.33*7e9)/(510998.95*510998.95)); // kinematic factor
+
+    auto aLong = TF1("aLong", "[1]*(1-x*(1+[0]))*((1-1/((1-x*(1-[0]))*(1-x*(1-[0]))))) / \
+    (((x*(1-[0]))*(x*(1-[0])))/(1-x*(1-[0])) + 1 + ((1-x*(1+[0]))/(1-x*(1-[0]))* \
+    ((1-x*(1+[0]))/(1-x*(1-[0])))))", x_min, x_max, "GL"); // longitudinal asymmetry
+
+    auto g = TF1("g", "aLong(f1(x))", x_min, x_max);
+
+    TFile *input = new TFile("bP_0.2.root", "read");
+    TTree *t = (TTree*)input->Get("t");
+
+    g.FixParameter(0, a);
+    g.SetParameter(1, 1); // polarization
+    g.SetParLimits(1, 0, 1);
+
+    g.SetParameter(2, -451500); // value of x where E = E_max
+    //g.SetParLimits(2, -451500, -451450);
+    g.SetParameter(3, 451500-451720); // del x, difference between max and min of x
+
+    gStyle->SetOptFit(1);
+    t->Draw("pXsec/uXsec:det.2_hits.x", "det.2_hits.x > -451715 & det.2_hits.x < -451500", "prof"); // profile plot of long asymmetry
+    t->Fit("g", "", "", "-451715", "-451500");
+    g.Draw("same");
 }
